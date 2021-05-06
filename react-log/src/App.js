cb25abf58f6e2876da51f8e5c1ad9e361a6c67a9
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-05-06 20:50:46 +0300

    adding-logout

--- react-log/src/App.js
+++ react-log/src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
 import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const {isLoading} = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>
   return (
+    <div>
    <LoginButton/>
+   <LogoutButton/>
+   </div>
   );
 }
 
