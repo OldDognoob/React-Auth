4a80c1aa98b5a0bff8a1312f862b64e718742856
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-05-06 22:51:05 +0300

    adding-profile

--- react-log/src/App.js
+++ react-log/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import { useAuth0 } from '@auth0/auth0-react';
+import Profile from './components/Profile';
 
 function App() {
   const {isLoading} = useAuth0();
@@ -12,6 +13,7 @@ function App() {
     <div>
    <LoginButton/>
    <LogoutButton/>
+   <Profile/>
    </div>
   );
 }
