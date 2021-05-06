f98cdde2ca4727a5abac282201fcbd8cbfb0e5f9
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-05-06 22:59:28 +0300

    wip-project

--- react-log/src/App.js
+++ react-log/src/App.js
@@ -1,20 +1,21 @@
-
+import React from 'react';
 import './App.css';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
-import { useAuth0 } from '@auth0/auth0-react';
 import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
-  const {isLoading} = useAuth0();
+  const { isLoading } = useAuth0();
 
   if (isLoading) return <div>Loading...</div>
+
   return (
-    <div>
-   <LoginButton/>
-   <LogoutButton/>
-   <Profile/>
-   </div>
+    <>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </>
   );
 }
 
