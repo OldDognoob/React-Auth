f30be21b8ef38c8cce6f027400984a145e7cf104
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-05-09 22:33:49 +0300

    adding-comments-to-index-file

--- react-log/src/index.js
+++ react-log/src/index.js
@@ -6,6 +6,10 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
+// when we import authProvider it wraps our app. 
+// Because authProvider component use React context will be able to access
+// all of its properties within ou app. And it will take some parameters
+// Its going to pass our current url, location to our browser.
 ReactDOM.render(
   <Auth0Provider
     domain={domain}
